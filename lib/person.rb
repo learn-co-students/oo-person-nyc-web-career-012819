1763976bb1e736d919f819500b7c095d78bb1de3
Elizabeth Kosowski <elizabethkosowski@gmail.com>
2019-01-30 11:59:47 -0500

    8 specs left

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,80 @@
-# your code goes here
+require 'pry'# your code goes here
+class Person
+
+  attr_reader :name, :happiness, :hygiene
+  attr_accessor :bank_account, :topic
+
+  def initialize(name, bank_account=25, happiness=8, hygiene=8)
+    @name = name
+    @bank_account = bank_account
+    @happiness = happiness
+    @hygiene = hygiene
+  end
+
+  def happiness=(happiness)
+    if happiness > 10
+      @happiness = 10
+    elsif happiness < 0
+      @happiness = 0
+    else
+      @happiness = happiness
+    end
+  end
+
+  def hygiene=(hygiene)
+    if hygiene > 10
+      @hygiene = 10
+    elsif hygiene < 0
+      @hygiene = 0
+    else
+      @hygiene = hygiene
+    end
+  end
+
+  def happy?
+    if @happiness > 7
+      return true
+    else
+      return false
+    end
+  end
+
+  def clean?
+    if @hygiene > 7
+      return true
+    else
+      return false
+    end
+  end
+
+  def get_paid(salary)
+    @bank_account += salary
+    return "all about the benjamins"
+  end
+
+  def take_bath
+    self.hygiene += 4
+    return "♪ Rub-a-dub just relaxing in the tub ♫"
+  end
+
+  def work_out
+    self.hygiene -= 3
+    self.happiness += 2
+    return "♪ another one bites the dust ♫"
+  end
+
+  def call_friend(friend)
+    self.happiness += 3
+    friend.happiness += 3
+    return "Hi #{friend.name}! It's #{@name}. How are you?"
+  end
+
+  def start_conversation(person, topic)
+    if @topic == "politics"
+      return 'blah blah partisan blah lobbyist'
+      self.happiness -= 2
+      person.happiness -= 2
+    end
+  end
+
+end
